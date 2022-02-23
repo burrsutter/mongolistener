f7ed1b7c48594e123fa2a6bd9b36b9d9da58447f
Burr Sutter <burrsutter@gmail.com>
2022-02-23 07:14:21 -0500

    adding postdestroy and sentinel

--- src/main/java/com/redhat/demo/MongoStreamListenerWSFruit.java
+++ src/main/java/com/redhat/demo/MongoStreamListenerWSFruit.java
@@ -1,12 +1,13 @@
 package com.redhat.demo;
 
 
-import java.util.Collections;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Consumer;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -15,20 +16,15 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-import com.mongodb.client.model.Aggregates;
-import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.changestream.ChangeStreamDocument;
-import com.mongodb.client.model.changestream.FullDocument;
 
-
-import org.bson.conversions.Bson;
 import org.jboss.logging.Logger;
 
-import io.quarkus.mongodb.ChangeStreamOptions;
 import io.quarkus.mongodb.reactive.ReactiveMongoClient;
 import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
 import io.quarkus.mongodb.reactive.ReactiveMongoDatabase;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.subscription.Cancellable;
 
 @ServerEndpoint("/mongofruits")
 
@@ -38,34 +34,59 @@ public class MongoStreamListenerWSFruit {
     
     private final List<Session> sessions = new CopyOnWriteArrayList<>();
 
+    private Cancellable cancellable;
+
+    static ChangeStreamDocument<Fruit> SENTINEL = new ChangeStreamDocument<>(null, null, null, null, null, null, null, null, null, null);
+
+        
     @Inject 
     ReactiveMongoClient mongoClient;
 
     @PostConstruct
     public void setup() {
-        /* Mongo Change Streams Listener Stuff */  
-        
-
+        /* Mongo Change Streams Listener Stuff */
         ReactiveMongoDatabase database = mongoClient.getDatabase("fruit");
         ReactiveMongoCollection<Fruit> dataCollection = database.getCollection("fruit", Fruit.class);
         
         
         Multi<ChangeStreamDocument<Fruit>> watcher = dataCollection.watch(Fruit.class);
+        Multi<ChangeStreamDocument<Fruit>> periodic = Multi.createFrom().ticks().every(Duration.ofSeconds(10))
+        .map(x -> SENTINEL);
 
 
-        watcher.subscribe().with(this.receiveChanges());
+        this.cancellable = Multi.createBy().merging().streams(watcher, periodic).subscribe().with(s -> this.receiveChanges(s), f -> LOG.error("Unable to consume the stream", f));
+
+        // watcher.subscribe().with(this.receiveChanges());
                    
     }
 
-    public Consumer<ChangeStreamDocument<Fruit>> receiveChanges() {
-      return message -> { 
-        LOG.info("Message: " + message);
-        Fruit fruit = message.getFullDocument();
+    @PreDestroy
+    public void cleanup() {
+        if (cancellable == null) {
+            cancellable.cancel();
+            cancellable = null;
+        }
+    }
+
+    public void receiveChanges(ChangeStreamDocument<Fruit> change) {
+      LOG.info("Change: " + change);
+      if (change != null) {
+        Fruit fruit = change.getFullDocument();
         String toBeSent = fruit.getName() + ":" + fruit.getDescription();
         LOG.info(toBeSent);
-	    broadcast(toBeSent);
-      };
+        broadcast(toBeSent); // sent out via the websocket connections  
+      }
     }
+
+    // public Consumer<ChangeStreamDocument<Fruit>> receiveChanges() {
+    //   return change -> { 
+    //     LOG.info("Cessage: " + change);
+    //     Fruit fruit = change.getFullDocument();
+    //     String toBeSent = fruit.getName() + ":" + fruit.getDescription();
+    //     LOG.info(toBeSent);
+	  //   broadcast(toBeSent); // sent out via the websocket connections
+    //   };
+    // }
 
     /* Websockets Stuff */
     @OnOpen
